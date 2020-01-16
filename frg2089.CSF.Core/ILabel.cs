aa0d2e02d0411e3c5bf5df52ad8e780526de5d78
舰队的偶像-岛风酱! <frg2089@outlook.com>
2020-01-16 13:25:46 +0800

    创建标签接口

---
+++ frg2089.CSF.Core/ILabel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core
+{
+    public interface ILabel
+    {
+        ///// <summary>
+        ///// 标签标记 始终为 " LBL"
+        ///// </summary>
+        //string LabelTag { get; }
+        /// <summary>
+        /// 字符串数
+        /// </summary>
+        int StringNum { get; }
+        /// <summary>
+        /// 标签名长度(ASCII)
+        /// </summary>
+        int LabelLength { get; }
+        /// <summary>
+        /// 标签名字符串
+        /// </summary>
+        string LabelName { get; set; }
+        /// <summary>
+        /// 字符串值标记 为" RTS"或"WRTS"
+        /// </summary>
+        string ValueTag { get; }
+        /// <summary>
+        /// 字符串值集合
+        /// </summary>
+        IEnumerable<IValue> Values { get; set; }
+        /// <summary>
+        /// 额外值长度(ASCII)
+        /// </summary>
+        int? ExtraValueLength { get; }
+        /// <summary>
+        /// 额外值字符串
+        /// </summary>
+        string ExtraValue { get; set; }
+    }
+}
---
+++ frg2089.CSF.Core/IValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core
+{
+    /// <summary>
+    /// 字符串 值
+    /// </summary>
+    public interface IValue
+    {
+        /// <summary>
+        /// 字符串 值长度(Unicode 即 实际长度/2)
+        /// </summary>
+        int ValueLength { get; }
+        /// <summary>
+        /// 字符串 值字符串
+        /// </summary>
+        string ValueString { get; set; }
+    }
+}
