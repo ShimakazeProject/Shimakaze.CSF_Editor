38fbbe3c9ff5550285651b17f4d2ac479232be40
舰队的偶像-岛风酱! <frg2089@outlook.com>
2020-01-16 15:03:41 +0800

    添加长度属性

---
+++ frg2089.CSF.Core/Helper/LabelHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core.Helper
+{
+    public class LabelHelper : ILabel
+    {
+        public string LabelTag { get; private set; }
+
+        public int StringCount { get; private set; }
+
+        public int NameLength { get; private set; }
+
+        public string LabelName { get; set; }
+
+        public IEnumerable<IValue> Values { get; set; }
+
+        public int Length => 0x0C + NameLength + (from value in Values select value.Length).Sum();
+
+        private LabelHelper(string labelTag, int stringCount, int nameLength, string labelName, IEnumerable<IValue> values)
+        {
+            LabelTag = labelTag;
+            StringCount = stringCount;
+            NameLength = nameLength;
+            LabelName = labelName;
+            Values = values;
+        }
+
+        public static ILabel CreateLabel(IEnumerable<byte> label)
+        {
+            var tag = Encoding.ASCII.GetString(label.Skip(0x00).Take(4).ToArray());
+            var stringCount = BitConverter.ToInt32(label.Skip(0x04).Take(4).ToArray(), 0);
+            var nameLength = BitConverter.ToInt32(label.Skip(0x08).Take(4).ToArray(), 0);
+            var labelName = Encoding.ASCII.GetString(label.Skip(0x0C).Take(nameLength).ToArray());
+            var values = new IValue[stringCount];
+            int start = 0x0C + nameLength;
+            for (int i = 0; i < stringCount; i++)
+            {
+                var value = ValueHelper.CreateValue(label.Skip(start));
+                start += value.Length;
+                values[i] = value;
+            }
+            return new LabelHelper(tag, stringCount, nameLength, labelName, values);
+        }
+    }
+}
---
+++ frg2089.CSF.Core/Helper/ValueHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core.Helper
+{
+    public class ValueHelper : IValue
+    {
+
+        private ValueHelper(string valueTag,int valueLength,string valueString,int? extraLength,string extraString)
+        {
+            ValueTag = valueTag;
+            ValueLength = valueLength;
+            ValueString = valueString;
+            ExtraLength = extraLength;
+            ExtraString = extraString;
+        }
+
+        public string ValueTag { get; private set; }
+
+        public int ValueLength { get; private set; }
+
+        public string ValueString { get; set; }
+
+        public int? ExtraLength { get; private set; }
+
+        public string ExtraString { get; set; }
+
+        public int Length => (ValueLength * 2) + 0x0C + ExtraLength ?? 0;
+
+        public static IValue CreateValue(IEnumerable<byte> value)
+        {
+            var vlength = BitConverter.ToInt32(value.Skip(0x04).Take(4).ToArray(), 0);
+            var elength = BitConverter.ToInt32(value.Skip(0x08 + vlength * 2).Take(4).ToArray(), 0);
+            return new ValueHelper(Encoding.ASCII.GetString(value.Skip(0x00).Take(4).ToArray()),
+                                   vlength,
+                                   Encoding.Unicode.GetString(Decoding(vlength, value.Skip(0x08).Take(vlength * 2).ToArray())),
+                                   elength,
+                                   Encoding.ASCII.GetString(value.Skip(0x0C + (vlength * 2)).Take(elength).ToArray()));
+        }
+        /// <summary>
+        /// 编/解码
+        /// </summary>
+        /// <param name="ValueLength">长度</param>
+        /// <param name="ValueData">内容</param>
+        /// <returns>编/解码后的数组</returns>
+        private static byte[] Decoding(int ValueLength, byte[] ValueData)
+        {
+            int ValueDataLength = ValueLength << 1;
+            for (int i = 0; i < ValueDataLength; ++i)
+            {
+                ValueData[i] = (byte)~ValueData[i];
+            }
+            return ValueData;
+        }
+    }
+}
