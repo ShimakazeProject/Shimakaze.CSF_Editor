18c7b4dd697729ffe4803c42b824e827f4e010b7
舰队的偶像-岛风酱! <frg2089@outlook.com>
2020-01-16 13:54:47 +0800

    HeaderHelper类完成

---
+++ frg2089.CSF.Core/Helper/HeaderHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core.Helper
+{
+    public class HeaderHelper : IHeader
+    {
+        public string Flag { get; private set; }
+
+        public int Version { get; private set; }
+
+        public int LabelCount { get; private set; }
+
+        public int StringCount { get; private set; }
+
+        public IEnumerable<byte> Unknow { get; private set; }
+
+        public int Language { get; private set; }
+
+        private HeaderHelper(string flag,int version,int labelCount,int stringCount, IEnumerable<byte> unknow,int language)
+        {
+            Flag = flag;
+            Version = version;
+            LabelCount = labelCount;
+            StringCount = stringCount;
+            Unknow = unknow;
+            Language = language;
+        }
+
+        public static IHeader CreateHeader(IEnumerable<byte> bytes)
+        {
+            if (bytes.Count()==24)
+            {
+                return new HeaderHelper(Encoding.ASCII.GetString(bytes.Take(0x00).Skip(4).ToArray()),
+                                        BitConverter.ToInt32(bytes.Take(0x04).Skip(4).ToArray(), 0),
+                                        BitConverter.ToInt32(bytes.Take(0x08).Skip(4).ToArray(), 0),
+                                        BitConverter.ToInt32(bytes.Take(0x0C).Skip(4).ToArray(), 0),
+                                        bytes.Take(0x10).Skip(4),
+                                        BitConverter.ToInt32(bytes.Take(0x14).Skip(4).ToArray(), 0));
+            }
+            else
+            {
+                throw new FormatException("It's Not A Header Bytes");
+            }
+        }
+    }
+}
