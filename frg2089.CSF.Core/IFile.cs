73507ffce74a4309bda7c46b4e07336125e15cc1
舰队的偶像-岛风酱! <frg2089@outlook.com>
2020-01-16 13:35:18 +0800

    创建文件 文件头接口

---
+++ frg2089.CSF.Core/IFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core
+{
+    public interface IFile
+    {
+        /// <summary>
+        /// 文件头
+        /// </summary>
+        IHeader Header { get; }
+        /// <summary>
+        /// 标签列表
+        /// </summary>
+        IEnumerable<ILabel> Label { get; }
+
+    }
+}
---
+++ frg2089.CSF.Core/IHeader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frg2089.CSF.Core
+{
+    /// <summary>
+    /// 文件头信息
+    /// </summary>
+    public interface IHeader
+    {
+        /// <summary>
+        /// 文件标记 始终为" FSC"
+        /// </summary>
+        string Flag { get; }
+        /// <summary>
+        /// 文件版本
+        /// </summary>
+        int Version { get; }
+        /// <summary>
+        /// 标签数
+        /// </summary>
+        int NumLabel { get; }
+        /// <summary>
+        /// 字符串数
+        /// </summary>
+        int NumString { get; }
+        /// <summary>
+        /// 额外信息 4字节
+        /// </summary>
+        IEnumerable<byte> Message { get; }
+        /// <summary>
+        /// 语言信息
+        /// </summary>
+        int Language { get; }
+    }
+}
