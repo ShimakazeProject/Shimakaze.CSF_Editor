208588f90dc7fbe9b2ca4e6cdd4897c79c46ac0a
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-01-28 01:07:06 +0800

    =  =

--- CSF.WPF.Framework/ViewModel/DocumentVM.cs
+++ CSF.WPF.Framework/ViewModel/DocumentVM.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSF.WPF.Framework.ViewModel
 {
-    public class DocumentVM
+    public class DocumentVM: INotifyPropertyChanged
     {
+        public Model.Type[] Types { get; set; }
 
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
