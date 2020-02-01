38fa2d111df146cfd5435c0da3d709dadf78ec96
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-01 21:38:50 +0800

    测试

---
+++ CSF.Core/CsfHeader.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSF.Core
+{
+    public abstract class CsfHeader
+    {
+        /// <summary>
+        /// 文件标记 始终为" FSC"
+        /// </summary>
+        public abstract string Flag { get; }
+        /// <summary>
+        /// 文件版本
+        /// </summary>
+        public abstract int Version { get; }
+        /// <summary>
+        /// 标签数
+        /// </summary>
+        public abstract int LabelCount { get; }
+        /// <summary>
+        /// 字符串数
+        /// </summary>
+        public abstract int StringCount { get; }
+        /// <summary>
+        /// 额外信息 4字节
+        /// </summary>
+        public abstract byte[] Unknow { get; }
+        /// <summary>
+        /// 语言信息
+        /// </summary>
+        public abstract CsvLanguage Language { get; }
+        public static CsfHeader GetHeader(byte[] bytes)
+        {
+            using (var ms = new MemoryStream(bytes))
+                return GetHeader(ms);
+        }
+        public static CsfHeader GetHeader(Stream stream)
+        {
+            using (var br = new ByteReader(stream))
+                return GetHeader(br.Read(4), br.Read(4), br.Read(4), br.Read(4), br.Read(4), br.Read(4));
+        }
+        public static CsfHeader GetHeader(string flag, int version, int labelCount, int stringCount, byte[] unknow, int language) 
+            => GetHeader(flag, version, labelCount, stringCount, unknow, (byte)language);
+
+        public static CsfHeader GetHeader(int version, int labelCount, int stringCount, int language)
+            => GetHeader(" FSC", version, labelCount, stringCount, new byte[4], (byte)language);
+        public static CsfHeader GetHeader(byte[] flag, byte[] version, byte[] labelCount, byte[] stringCount, byte[] nuknow, byte[] language)
+            => GetHeader(Encoding.ASCII.GetString(flag), BitConverter.ToInt32(version, 0), BitConverter.ToInt32(labelCount, 0),
+                BitConverter.ToInt32(stringCount, 0), nuknow, BitConverter.ToInt32(language, 0));
+
+        /// <summary>
+        /// 最重要的GetHeader方法
+        /// </summary>
+        public static CsfHeader GetHeader(string flag, int version, int labelCount, int stringCount, byte[] unknow, byte language)
+            => new HeaderMaker(flag, version, labelCount, stringCount, unknow, (CsvLanguage)language);
+    }
+    public enum CsvLanguage : byte
+    {
+        US, ZERO1, GERMAN, FRENCH, ZERO2, ZERO3,
+        ZERO4, ZERO5, KOREAN, CHINESE
+    }
+    public class HeaderMaker : CsfHeader
+    {
+        private readonly string flag;
+        private readonly int version;
+        private readonly int labelCount;
+        private readonly int stringCount;
+        private readonly byte[] unknow;
+        private readonly CsvLanguage language;
+
+        public override string Flag => flag;
+        public override int Version => version;
+        public override int LabelCount => labelCount;
+        public override int StringCount => stringCount;
+        public override byte[] Unknow => unknow;
+        public override CsvLanguage Language => language;
+        public HeaderMaker(string flag, int version, int labelCount, int stringCount,
+            byte[] unknow, CsvLanguage language)
+        {
+            this.flag = flag;
+            this.version = version;
+            this.labelCount = labelCount;
+            this.stringCount = stringCount;
+            this.unknow = unknow;
+            this.language = language;
+        }
+    }
+}
---
+++ CSF.Core/CsfLabel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSF.Core
+{
+    public abstract class CsfLabel
+    {
+        public abstract string LabelTag { get; }
+
+        public abstract int StringCount { get; }
+
+        public abstract int NameLength { get; }
+
+        public abstract string LabelName { get; set; }
+
+        public abstract CsfValue[] Values { get; set; }
+
+        public static CsfValue GetValue(ByteReader byteReader)
+        {
+
+            var strTag = Encoding.ASCII.GetString(byteReader.Read(4));
+            var strLenght = BitConverter.ToInt32(byteReader.Read(4), 0);
+            var strValue = Encoding.Unicode.GetString(
+                CsfValue.Decoding(strLenght, byteReader.Read(strLenght * 2)));
+            if (strTag.Equals("WRTS"))
+            {
+                var strELenght = BitConverter.ToInt32(byteReader.Read(4), 0);
+                var strEValue = Encoding.ASCII.GetString(byteReader.Read(strELenght));
+                return GetValue(strTag, strLenght, strValue, strELenght, strEValue);
+            }
+            else //if (strTag.Equals(" RTS"))
+                return GetValue(strTag, strLenght, strValue);
+        }
+        public static CsfValue GetValue(string valueTag, int valueLength, string valueString, int? extraLength = null, string extraString = null)
+            => new ValueMaker(valueTag, valueLength, valueString, extraLength, extraString);
+
+        public static CsfLabel GetLabel(Stream stream)
+        {
+            using (var br = new ByteReader(stream))
+            {
+                var tag = Encoding.ASCII.GetString(br.Read(4));
+                var count = BitConverter.ToInt32(br.Read(4), 0);
+                var length = BitConverter.ToInt32(br.Read(4), 0);
+                var name = Encoding.ASCII.GetString(br.Read(4));
+                var valueArray = new CsfValue[length];
+                for (int i = 0; i < count; i++)
+                    valueArray[i] = GetValue(br);
+
+                return GetLabel(tag, count, length, name, valueArray);
+            }
+
+        }
+        public static CsfLabel GetLabel(byte[] labelTag, byte[] stringCount, byte[] nameLength, byte[] labelName, byte[] values)
+        {
+            var tag = Encoding.ASCII.GetString(labelTag);
+            var count = BitConverter.ToInt32(stringCount, 0);
+            var length = BitConverter.ToInt32(nameLength, 0);
+            var name = Encoding.ASCII.GetString(labelName);
+            var valueArray = new CsfValue[length];
+            using (var br = new ByteReader(new MemoryStream(values)))
+                for (int i = 0; i < count; i++)
+                    valueArray[i] = GetValue(br);
+
+            return GetLabel(tag, count, length, name, valueArray);
+        }
+        public static CsfLabel GetLabel(string labelTag, int stringCount, int nameLength, string labelName, CsfValue[] values)
+            => new LabelMaker(labelTag, stringCount, nameLength, labelName, values);
+    }
+    public class LabelMaker : CsfLabel
+    {
+        private readonly string labelTag;
+        private readonly int stringCount;
+        private readonly int nameLength;
+        private string labelName;
+        private CsfValue[] values;
+
+        public override string LabelTag => labelTag;
+        public override int StringCount => stringCount;
+        public override int NameLength => nameLength;
+        public override string LabelName { get => labelName; set => labelName = value; }
+        public override CsfValue[] Values { get => values; set => values = value; }
+        public LabelMaker(string labelTag, int stringCount, int nameLength, string labelName, CsfValue[] values)
+        {
+            this.labelTag = labelTag;
+            this.stringCount = stringCount;
+            this.nameLength = nameLength;
+            this.labelName = labelName;
+            this.values = values;
+        }
+    }
+}
