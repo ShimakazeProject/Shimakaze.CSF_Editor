116d245168d36c857f1a8c63c39b7e411543f4c2
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-05 22:46:22 +0800

    适配类库

--- CSF.WPF.Framework/ViewModel/DocumentVM.cs
+++ CSF.WPF.Framework/ViewModel/DocumentVM.cs
@@ -20,12 +20,12 @@ namespace CSF.WPF.Framework.ViewModel
         private Model.Type[] types;
         private Model.Type selectedType;
         private Model.Label selectedLabel;
-        private string typesNum;
-        private string labelsNum;
-        private string stringsNum;
+        private int typesCount;
         private string selectedTypeLabelsNum;
         private string selectedTypeStringsNum;
         private string selectedLabelStringsNum;
+        private int stringCount;
+        private int labelCount;
         #endregion
 
         public DocumentVM(Controls.Document document)
@@ -33,26 +33,92 @@ namespace CSF.WPF.Framework.ViewModel
             this.document = document;
             Types = Array.Empty<Model.Type>();
 
+            PropertyChanged += DocumentVM_PropertyChanged;
+
         }
 
+        private void DocumentVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //var stopwatch = new System.Diagnostics.Stopwatch();
+            //stopwatch.Start();
+            switch (e.PropertyName)
+            {
+                case nameof(Types):
+                    TypesCount = types.Length;// 类型总数
+                    break;
+                case nameof(SelectedType):
+                    SelectedTypeLabelsNum = selectedType?.Labels.Length.ToString();
+                    SelectedTypeStringsNum = selectedType != null ? (from label in selectedType.Labels select label.LabelValues.Length)?.Sum().ToString() : null;
+                    break;
+                case nameof(SelectedLabel):
+                    SelectedLabelStringNum = selectedLabel?.LabelStrCount.ToString();
+                    break;
+                default:
+                    break;
+            }
+            //stopwatch.Stop();
+            //Console.WriteLine("用时:{0}秒", ((double)stopwatch.ElapsedMilliseconds / 1000).ToString("0.000"));
+        }
+
+        /// <summary>
+        /// 文件标记 始终为" FSC"
+        /// </summary>
+        public string Flag { get; private set; }
+        /// <summary>
+        /// 文件版本
+        /// </summary>
+        public int Version { get; private set; }
+
+
+
+        /// <summary>
+        /// 额外信息 4字节
+        /// </summary>
+        public byte[] Unknow { get; private set; }
+        /// <summary>
+        /// 语言信息
+        /// </summary>
+        public CsfLanguage Language { get; private set; }
+
+        public void MakeType(Model.File file)
+        {
+            Flag = file.Flag;
+            Version = file.Version;
+            LabelCount = file.LabelCount;
+            StringCount = file.StringCount;
+            Unknow = file.Unknow;
+            Language = (CsfLanguage)file.Language;
+            var labels = file.Labels;
+            var types = new List<Model.Type>();
+            for (int i = 0; i < labels.Length; i++)
+            {
+                var label = labels[i];
+                var split = label.LabelName.Split(':', '_');
+                string typeName = split.Length > 1 ? split[0] : "(Default)";
+
+                bool finished = false;
+                for (int j = 0; j < types.Count; j++)
+                {
+                    if (types[j].Name.Equals(typeName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        types[j] += label;
+                        finished = true;
+                        break;
+                    }
+                }
+                if (!finished)
+                {
+                    types.Add(new Model.Type(typeName.ToUpper(), label));
+                }
+            }
+            Types = types.ToArray();
+        }
         #region Binding Propertys
         public Model.Type[] Types
         {
             get => types; set
             {
-                bool notNull = value != null;
                 types = value;
-                TypesNum = value?.Length.ToString();
-                LabelsNum = notNull ? (from type in value
-                                       select type.Labels.Count
-                                       ).Sum().ToString() : null;
-
-                StringsNum = notNull ? (from type in value // 从List中获得Type
-                                        select (from values in
-                                                    from label in type.Labels
-                                                    select label.Value
-                                                select values.Count
-                                        ).Sum()).Sum().ToString() : null;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Types)));
             }
         }
@@ -61,45 +127,53 @@ namespace CSF.WPF.Framework.ViewModel
             get => selectedType; set
             {
                 selectedType = value;
-                SelectedTypeLabelsNum = value?.Labels.Count.ToString();
-                SelectedTypeStringsNum = value != null ? (from label in value.Labels select label.Value.Count)?.Sum().ToString() : null;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedType)));
             }
         }
-
         public Model.Label SelectedLabel
         {
             get => selectedLabel; set
             {
                 selectedLabel = value;
-                SelectedLabelStringNum = value?.StringCount.ToString();
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedLabel)));
             }
         }
-        public string TypesNum
+
+
+        /// <summary>
+        /// 类型数
+        /// </summary>
+        public int TypesCount
         {
-            get => typesNum; set
+            get => typesCount; set
             {
-                typesNum = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypesNum)));
+                typesCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypesCount)));
             }
         }
-        public string LabelsNum
+        /// <summary>
+        /// 标签数
+        /// </summary>
+        public int LabelCount
         {
-            get => labelsNum; set
+            get => labelCount; private set
             {
-                labelsNum = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LabelsNum)));
+                labelCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LabelCount)));
             }
         }
-        public string StringsNum
+        /// <summary>
+        /// 字符串数
+        /// </summary>
+        public int StringCount
         {
-            get => stringsNum; set
+            get => stringCount; private set
             {
-                stringsNum = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StringsNum)));
+                stringCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StringCount)));
             }
         }
+
         public string SelectedTypeLabelsNum
         {
             get => selectedTypeLabelsNum; set
@@ -131,20 +205,25 @@ namespace CSF.WPF.Framework.ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Types)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedType)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedLabel)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypesNum)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LabelsNum)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StringsNum)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypesCount)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedTypeLabelsNum)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedTypeStringsNum)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedLabelStringNum)));
         });
 
-
-
-        public async void OpenCsf(string path)
+       public enum CsfLanguage : byte
         {
-            var task = Import.FromCSF(path);
-            Types = (await task).ToArray();
+            US, ZERO1, GERMAN, FRENCH, ZERO2, ZERO3,
+            ZERO4, ZERO5, KOREAN, CHINESE
+        }
+
+        public void OpenCsf(string path)
+        {
+            var file = new Model.File();
+            file.LoadFromFile(path);
+            MakeType(file);
+            //var task = Import.FromCSF(path);
+            //Types = (await task).ToArray();
             //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Types)));
         }
 
