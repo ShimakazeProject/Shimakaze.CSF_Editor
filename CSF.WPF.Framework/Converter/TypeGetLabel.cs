4775b6f04c744ed646b6ce2fd92859cfc4cb3524
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-06 18:28:58 +0800

    = . =

--- CSF.WPF.Framework/Converter/TypeGetLabel.cs
+++ CSF.WPF.Framework/Converter/TypeGetLabel.cs
@@ -22,35 +22,4 @@ namespace CSF.WPF.Framework.Converter
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
     }
-
-
-    [ValueConversion(typeof(Model.Label), typeof(string))]
-    public class LabelGetName : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value?.GetType() == typeof(Model.Label))
-            {
-                return (value as Model.Label).LabelName;
-            }
-            else return null;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
-    }
-
-    //[ValueConversion(typeof(Model.Value), typeof(List<Model.Value>))]
-    //public class TypeGetValue : IValueConverter
-    //{
-    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    //    {
-    //        if (value?.GetType() == typeof(Model.Type))
-    //        {
-    //            return (value as Model.Type).Labels.Values;
-    //        }
-    //        else return null;
-    //    }
-
-    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
-    //}
 }
