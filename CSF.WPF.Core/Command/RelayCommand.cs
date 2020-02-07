426d9d85f331ac2280f76e888768d822bc290917
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-07 13:51:52 +0800

    创建基于.Net Core 3.1(WPF)的项目

---
+++ CSF.WPF.Core/Command/RelayCommand.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CSF.WPF.Core.Command
+{
+    /// <summary>
+    /// 中继命令
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        //[DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+        public event EventHandler CanExecuteChanged
+        {   
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+    }
+
+    /// <summary>
+    /// 中继命令 泛型
+    /// </summary>
+    /// <typeparam name="T">泛型</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            if (canExecute == null)
+                canExecute = _ => true;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter) == false)
+                return;
+
+            OnExecute(parameter);
+        }
+
+        protected void OnExecute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
---
+++ CSF.WPF.Core/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSF.WPF.Core
+{
+    internal class Program
+    {
+
+        private const string GUID = "{FD25BA98-74C3-4793-976A-17A09CC7A2B6}";
+
+        /// <summary>
+        /// 程序的入口 主方法(主函数)
+        /// </summary>
+        /// <param name="args">命令行参数 不包括程序本身</param>
+        [STAThread] // 单线程模型 UI必备
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+#endif
+
+            var app = new App();
+            app.InitializeComponent();
+            app.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+            app.Run();
+        }
+    }
+}
