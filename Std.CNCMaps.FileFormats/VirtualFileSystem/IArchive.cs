317923250b5cf96f7c3528a6cae24342543f58b2
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-11 21:18:24 +0800

    移除不相关的项目

--- Std.CNCMaps.FileFormats/VirtualFileSystem/IArchive.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Std.CNCMaps.FileFormats.VirtualFileSystem {
-	public interface IArchive {
-		bool ContainsFile(string filename);
-		VirtualFile OpenFile(string filename, FileFormat format, CacheMethod m = CacheMethod.Default);
-
-		void Close();
-	}
-}
