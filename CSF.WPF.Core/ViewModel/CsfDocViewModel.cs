9506e14b1071e205ca498db371ce158ad2d55dc1
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-22 14:48:50 +0800

    创建VM

---
+++ CSF.WPF.Core/ViewModel/CsfDocViewModel.cs
@@ -0,0 +1,70 @@
+﻿using CSF.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CSF.WPF.Core.ViewModel
+{
+    public class CsfDocViewModel : INotifyPropertyChanged
+    {
+        private string filePath;
+        private Label data;
+        private Model.Type dataList;
+        private TypeSet typeList;
+
+        public CsfDocViewModel()
+        {
+            TypeList = new TypeSet();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Binding Property
+        public Label Data
+        {
+            get => data; set
+            {
+                data = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Data)));
+            }
+        }
+        public Model.Type DataList
+        {
+            get => dataList; set
+            {
+                dataList = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataList)));
+            }
+        }
+        public TypeSet TypeList
+        {
+            get => typeList; set
+            {
+                typeList = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TypeList)));
+            }
+        }
+        public string FilePath
+        {
+            get => filePath; set
+            {
+                filePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FilePath)));
+            }
+        }
+        #endregion
+
+        public void Open(string path)
+        {
+            FilePath = path;
+            var typelist = new TypeSet();
+            typelist.LoadFromFile(path);
+            TypeList = typelist;
+        }
+        public void SaveAs(string path)
+        {
+            FilePath = path;
+        }
+    }
+}
---
+++ CSF.WPF.Core/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using CSF.Logmgr;
+using CSF.WPF.Core.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Json;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CSF.WPF.Core.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CsfDoc document;
+        private List<MenuItem> importList;
+
+        public MainWindowViewModel()
+        {
+            Document = new CsfDoc();
+            try
+            {
+                ImportListInit();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Logger.Warn("未找到文件");
+            }
+        }
+
+        public CsfDoc Document
+        {
+            get => document; set
+            {
+                document = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Document)));
+            }
+        }
+        public List<MenuItem> ImportList
+        {
+            get => importList; set
+            {
+                importList = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImportList)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ImportListInit()
+        {
+            var list = new List<MenuItem>();
+            using var fs = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "\\Resources\\MenuItem.json", FileMode.Open, FileAccess.Read, FileShare.Read);
+            var config = JsonValue.Load(fs)["Import"] as JsonArray;
+            foreach (var item in config)
+            {
+                switch ((PluginType)Enum.Parse(typeof(PluginType), ((string)item["type"]).ToUpper(new System.Globalization.CultureInfo("en-US"))))
+                {
+                    case PluginType.REFLECTION:
+                        //System.Reflection.
+                        break;
+                    case PluginType.RUN:
+                        break;
+                    case PluginType.CALL:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            ImportList = list;
+        }
+        public enum PluginType
+        {
+            REFLECTION, // 反射程序集
+            RUN,    // 运行
+            CALL,   // 调用DLL
+        }
+    }
+}
