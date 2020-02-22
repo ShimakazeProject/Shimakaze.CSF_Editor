5f9cc32e9eea0ecdabc305eca22f053d36dd14df
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-02-22 14:49:17 +0800

    创建View

---
+++ CSF.WPF.Core/View/CsfDoc.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSF.WPF.Core.View
+{
+    /// <summary>
+    /// CsfDoc.xaml 的交互逻辑
+    /// </summary>
+    public partial class CsfDoc 
+    {
+        public CsfDoc()
+        {
+            InitializeComponent();
+            //DataContext = new ViewModel.CsfDocViewModel();
+        }
+    }
+}
