e9f151f658a09bc80703ad11388c84130a0c49b0
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-03-01 01:52:10 +0800

    修正BUG

--- CSF.WPF.Core/Program.cs
+++ CSF.WPF.Core/Program.cs
@@ -27,24 +27,25 @@ namespace CSF.WPF.Core
             Logger.Info("Operating system: " + Environment.OSVersion.VersionString);
 
             var app = new App();
+            app.DispatcherUnhandledException += App_DispatcherUnhandledException;
             app.InitializeComponent();
+            //app.StartupUri = new Uri("View/Window1.xaml", UriKind.Relative);
             app.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
-            //try
-            //{
-                app.Run();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Logger.Fatal("遭遇未经处理的异常:{0}", ex);
-            //    System.IO.File.Copy(AppContext.BaseDirectory + "Debug.log", AppContext.BaseDirectory + string.Format("Error{0}.log", DateTime.Now.ToString("MM.dd_HH_mmss.fff")));
-            //    var msgboxret = System.Windows.MessageBox.Show(string.Format("程序遭遇未经处理的异常{0}请将{2}目录下的错误日志提交给开发者{0}{3}{0}您是否需要跳转到问题反馈?{0}异常信息:{1}"
-            //        , Environment.NewLine, ex, AppContext.BaseDirectory, "https://github.com/frg2089/CNC-Strings-File-Editor/issues"),
-            //        "Fatal Exception", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Error);
-            //    if (msgboxret== System.Windows.MessageBoxResult.Yes)
-            //    {
+            app.Run();
+        }
 
-            //    }
-            //}
+        private static void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Logger.Fatal("遭遇未经处理的异常:{0}", e.Exception);
+            System.IO.File.Copy(AppContext.BaseDirectory + "Debug.log", AppContext.BaseDirectory + string.Format("Error{0}.log", DateTime.Now.ToString("MM.dd_HH_mmss.fff")));
+            var msgboxret = System.Windows.MessageBox.Show(string.Format("程序遭遇未经处理的异常{0}请将{2}目录下的错误日志提交给开发者{0}{3}{0}您是否需要跳转到问题反馈?{0}异常信息:{1}"
+                , Environment.NewLine, e.Exception.Message, AppContext.BaseDirectory, "https://github.com/frg2089/CNC-Strings-File-Editor/issues"),
+                "Fatal Exception", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Error);
+            if (msgboxret == System.Windows.MessageBoxResult.Yes)
+            {
+
+            }
+            throw new NotImplementedException();
         }
     }
 }
