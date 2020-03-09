6834b4726caf62d7ca30de5f1c641993a658d8e4
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-03-09 21:22:27 +0800

    把转换器改为静态的...

--- CSF.WPF.Core/Converter/TypeToStringNumberConverter.cs
+++ CSF.WPF.Core/Converter/TypeToStringNumberConverter.cs
@@ -7,17 +7,27 @@ using System.Windows.Data;
 
 namespace CSF.WPF.Core.Converter
 {
+    /// <summary>
+    /// 从Type获取字符串对数的转换器
+    /// </summary>
     [ValueConversion(typeof(Model.Type), typeof(int))]
     public class TypeToStringNumberConverter : IValueConverter
     {
+        public static TypeToStringNumberConverter Instance { get; private set; } = new TypeToStringNumberConverter();
+        private TypeToStringNumberConverter() { }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is null ? null : (int?)(from Model.Label label in ((Model.Type)value).Labels select label.LabelStrCount).Sum();
+            return 
+                value is null 
+                    ? null 
+                    : (int?)
+                        (from Model.Label label
+                         in ((Model.Type)value).Labels
+                         select label.LabelStrCount
+                         ).Sum();
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
     }
 }
