8e110f52985be73150d8826115f504a052a482d5
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-03-09 20:36:36 +0800

    给Ares一个面子

--- CSF.Model/CsfLanguage.cs
+++ CSF.Model/CsfLanguage.cs
@@ -1,8 +1,9 @@
 ﻿namespace CSF.Model
 {
-    public enum CsfLanguage:byte
+    public enum CsfLanguage : uint
     {
         US, ZERO1, GERMAN, FRENCH, ZERO2, ZERO3,
-        ZERO4, ZERO5, KOREAN, CHINESE
+        ZERO4, ZERO5, KOREAN, CHINESE,
+        NEUTRAL= 0xFF_FF_FF_FF
     }
 }
