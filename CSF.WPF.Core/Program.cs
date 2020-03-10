0f83a52a39a247cc00971862d4c796772f53d033
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-03-11 00:33:58 +0800

    = . =

--- CSF.WPF.Core/Program.cs
+++ CSF.WPF.Core/Program.cs
@@ -10,6 +10,8 @@ namespace CSF.WPF.Core
         public const string GUID = "{FD25BA98-74C3-4793-976A-17A09CC7A2B6}";
 
         public const ushort BUILD = 20038;
+
+        public static App App { get; private set; }= new App();
         /// <summary>
         /// 程序的入口 主方法(主函数)
         /// </summary>
@@ -28,20 +30,28 @@ namespace CSF.WPF.Core
             Logger.Info("Operating system: {0}", Environment.OSVersion.VersionString);
             Logger.Info("Is x86_64 OS: {0}", Environment.Is64BitOperatingSystem);
             Logger.Info("Is x86_64 Process: {0}", Environment.Is64BitProcess);
-
-            var app = new App();
-            app.InitializeComponent();
+            Logger.Info("======================");
+            Logger.Trace("程序初始化");
+            App.InitializeComponent();
             //MahApps.Metro.ThemeManager.ChangeTheme(app, "Green", "Dark");
-            app.DispatcherUnhandledException += App_DispatcherUnhandledException;
-            app.StartupUri = new Uri("View/MainWindow.xaml", UriKind.Relative);
-            app.Run();
+            App.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            App.Exit += App_Exit;
+            App.StartupUri = new Uri("View/MainWindow.xaml", UriKind.Relative);
+            Logger.Trace("程序运行");
+            App.Run();
+            Logger.Trace("程序正常结束");
+        }
+
+        private static void App_Exit(object sender, System.Windows.ExitEventArgs e)
+        {
+            Logger.Info("Exit :{0}", Environment.ExitCode);
         }
 
         private static void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             var LogFile = string.Format("{0}Error{1}.log", AppContext.BaseDirectory, DateTime.Now.ToString("_MM.dd_HH.mm.ss.fff"));
             const string url = "https://github.com/frg2089/CNC-Strings-File-Editor/issues";
-            Logger.Fatal("遭遇未经处理的异常:{0}", e.Exception);
+            Logger.Fatal("Dispatcher Unhandled Exception:{0}", e.Exception);
             System.IO.File.Copy(AppContext.BaseDirectory + "Debug.log", LogFile);
             var msgboxret = System.Windows.MessageBox.Show(
                 string.Format("程序遭遇未经处理的异常{0}您可以将错误日志{2}提交到{0}{3}{0}您是否需要跳转到问题反馈?{0}异常信息:{1}",
