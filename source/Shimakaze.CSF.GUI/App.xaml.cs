15f79a2dbdb7b3cfaf83d5a83b0a555b69e08187
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-05-09 22:07:31 +0800

    应用

--- source/Shimakaze.CSF.GUI/App.xaml.cs
+++ source/Shimakaze.CSF.GUI/App.xaml.cs
@@ -37,10 +37,35 @@ namespace Shimakaze.CSF.GUI
         protected override void OnStartup(StartupEventArgs e)
         {
             ColorThemeChange(isDarkTheme);
-            // now set the Green color scheme and dark base color
-            //Fluent.ThemeManager.ChangeTheme(Application.Current, "Dark.Green");
 
             base.OnStartup(e);
+            int timer = 3000;
+
+            //initialize the splash screen and set it as the application main window
+            var splashScreen = new SplashWindow();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            //in order to ensure the UI stays responsive, we need to
+            //do the work on a different thread
+            Task.Factory.StartNew(() =>
+            {
+                //simulate some work being done
+                System.Threading.Thread.Sleep(timer);
+
+                //since we're not on the UI thread
+                //once we're done we need to use the Dispatcher
+                //to create and show the main window
+                this.Dispatcher.Invoke(() =>
+                {
+                    //initialize the main window, set it as the application main window
+                    //and close the splash screen
+                    var mainWindow = new MainWindow();
+                    this.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    splashScreen.Close();
+                });
+            });
         }
         private void ColorThemeChange(bool dark)
         {
--- source/Shimakaze.CSF.GUI/MainWindow.xaml.cs
+++ source/Shimakaze.CSF.GUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,5 +35,6 @@ namespace Shimakaze.CSF.GUI
                 (sender as Fluent.Button).Header = "DarkTheme";
 
         }
+
     }
 }
