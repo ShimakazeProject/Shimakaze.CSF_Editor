e756209bba76201000fd2bd9e80f88661266dd80
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-05-09 21:53:49 +0800

    创建SplashWindow

---
+++ source/Shimakaze.CSF.GUI/SplashWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Shimakaze.CSF.GUI
+{
+    /// <summary>
+    /// StartWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await new System.Threading.Tasks.Task(() => System.Threading.Thread.Sleep(3000));
+            Close();
+        }
+    }
+}
