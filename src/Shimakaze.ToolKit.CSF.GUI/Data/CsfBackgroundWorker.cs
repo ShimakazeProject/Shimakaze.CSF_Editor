88dbe5091ab0dbc7df0b086d5b00413850dec645
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-06-23 10:33:04 +0800

    重构

--- src/Shimakaze.ToolKit.CSF.GUI/Data/CsfBackgroundWorker.cs
+++ src/Shimakaze.ToolKit.CSF.GUI/Data/CsfBackgroundWorker.cs
@@ -11,7 +11,7 @@ using Shimakaze.ToolKit.CSF.Kernel.Extension;
 
 namespace Shimakaze.ToolKit.CSF.GUI.Data
 {
-    public class ParseBackgroundWorker<T> : INotifyPropertyChanged
+    public class CsfBackgroundWorker<T> : INotifyPropertyChanged
         where T : CsfFileStruct, new()
     {
         private string statusString;
@@ -70,7 +70,7 @@ namespace Shimakaze.ToolKit.CSF.GUI.Data
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Error)));
             }
         }
-        public delegate void ParseBackgroundWorkerFinishedHander(ParseBackgroundWorker<T> sender, T result);
+        public delegate void ParseBackgroundWorkerFinishedHander(CsfBackgroundWorker<T> sender, T result);
         public event ParseBackgroundWorkerFinishedHander Finished;
         public event PropertyChangedEventHandler PropertyChanged;
         public async void BeginParse(Stream stream)
@@ -81,32 +81,20 @@ namespace Shimakaze.ToolKit.CSF.GUI.Data
 
             // 变量声明区
             T file = new T();// 创建实例
-            int h_version, h_labelCount, h_stringCount, h_unknown, h_language,
-                l_count, l_nameLength,
-                v_length, v_extraLength,
+            int h_labelCount, l_count, l_nameLength,
                 this_label_number, this_value_number;
-            string h_flag, l_flag, l_name, v_flag, v_content, v_extra;
+            string  l_flag, l_name;
             CsfLabelStruct tmp_label;
-            CsfValueStruct tmp_value;
             // ====================================================
             if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
             try
             {
-                StatusString = "正在读取文件头";
                 // 获取文件头
-                h_flag = Encoding.ASCII.GetString(await stream.ReadAsync(4));
-                if (!h_flag.Equals(CsfHeadStruct.CSF_FLAG)) throw new FormatException("Unknown File Format: Flag is NOT True");
 
-                // 文件版本
-                h_version = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                // 文件标签数
-                h_labelCount = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                // 文件字符串数   满进程
-                h_stringCount = MaxProgress = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                // 不明内容
-                h_unknown = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                // 语言
-                h_language = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
+                StatusString = "正在读取文件头";
+                file.Head = await CsfHeadStruct.ParseAsync(stream);
+                h_labelCount = file.Head.LabelCount;
+                MaxProgress = file.Head.StringCount;
 
                 UnknowProgress = false;
                 Progress = 0;
@@ -119,7 +107,8 @@ namespace Shimakaze.ToolKit.CSF.GUI.Data
                     // 标签头
                     l_flag = Encoding.ASCII.GetString(await stream.ReadAsync(4));
                     // 标签头对不上
-                    if (!l_flag.Equals(CsfLabelStruct.CSF_LABEL_FLAG)) throw new FormatException("Unknown File Format");
+                    if (!l_flag.Equals(CsfLabelStruct.CSF_LABEL_FLAG))
+                        throw new FormatException(string.Format("Unknown File Format: Label[{0}] Flag is NOT True", this_label_number));
 
                     // 字符串数量 
                     l_count = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
@@ -134,34 +123,13 @@ namespace Shimakaze.ToolKit.CSF.GUI.Data
                     for (this_value_number = 0; this_value_number < l_count; this_value_number++)
                     {
                         if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
-                        // 字符串标记
-                        v_flag = Encoding.ASCII.GetString(await stream.ReadAsync(4));
-                        // 字符串主要内容长度
-                        v_length = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                        // 字符串主要内容
-                        v_content = Encoding.Unicode.GetString(
-                            CsfValueStruct.Decoding(
-                                await stream.ReadAsync(v_length << 1)));
 
-                        tmp_value = new CsfValueStruct(v_content);
-                        // 判断是否包含额外内容
-                        if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
-                        if (v_flag.Equals(CsfValueStruct.CSF_VALUE_WSTR))// 存在额外内容
-                        {
-                            tmp_value.IsWstr = true;
-                            // 额外内容长度
-                            v_extraLength = BitConverter.ToInt32(await stream.ReadAsync(4), 0);
-                            // 额外内容
-                            tmp_value.Extra = v_extra = Encoding.ASCII.GetString(await stream.ReadAsync(v_extraLength));
-                        }
+                        tmp_label.Add(await CsfValueStruct.ParseAsync(stream));
                         Progress++;
-                        if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
-                        tmp_label.Add(tmp_value);
                     }
                     if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
-                    file.Add(tmp_label);
+                    file.AddNoChangeHead(tmp_label);
                 }
-                file.Head = new CsfHeadStruct(h_version, h_labelCount, h_stringCount, h_unknown, h_language);
                 result = file;
             }
             catch (TaskCanceledException)
@@ -187,6 +155,83 @@ namespace Shimakaze.ToolKit.CSF.GUI.Data
         public void Cancel()
         {
             TaskStatus = TaskStatus.Canceled;
+        }
+
+        public async void BeginDeparse(Stream stream, T file)
+        {
+            if (file is null)
+            {
+                Error = new NullReferenceException();
+                TaskStatus = TaskStatus.Faulted;
+                return;
+            }
+            if (TaskStatus == TaskStatus.Running) return;// 重复运行
+            else TaskStatus = TaskStatus.Running;
+            const string progressStr = "正在写入第{0}/{1}个标签";
+
+            // 变量声明区
+            int h_labelCount, l_count,
+                this_label_number, this_value_number;
+            CsfLabelStruct tmp_label;
+            // ====================================================
+            if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+            try
+            {
+                StatusString = "正在写入文件头";
+                await file.Head.DeparseAsync(stream);
+                h_labelCount = file.Head.LabelCount;
+                MaxProgress = file.Head.StringCount;
+                UnknowProgress = false;
+                Progress = 0;
+                if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+                for (this_label_number = 0; this_label_number < h_labelCount; this_label_number++)
+                {
+                    if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+                    StatusString = string.Format(progressStr, this_label_number, h_labelCount);
+
+                    tmp_label = file[this_label_number];
+
+                    // 标签头
+                    await stream.WriteAsync(Encoding.ASCII.GetBytes(CsfLabelStruct.CSF_LABEL_FLAG));
+                    // 字符串数量 
+                    await stream.WriteAsync(BitConverter.GetBytes(tmp_label.Count));
+                    // 标签名长度
+                    await stream.WriteAsync(BitConverter.GetBytes(tmp_label.Name.Length));
+                    // 标签名
+                    await stream.WriteAsync(Encoding.ASCII.GetBytes(tmp_label.Name));
+
+                    l_count = tmp_label.Count;
+
+                    if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+                    for (this_value_number = 0; this_value_number < l_count; this_value_number++)
+                    {
+                        await tmp_label[this_value_number].DeparseAsync(stream);
+
+                        Progress++;
+                        if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+                    }
+                    if (TaskStatus == TaskStatus.Canceled) throw new TaskCanceledException();
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                TaskStatus = TaskStatus.Canceled;
+                return;
+            }
+            catch (Exception e)
+            {
+                Error = e;
+                TaskStatus = TaskStatus.Faulted;
+                Finished?.Invoke(this, result);
+                return;
+            }
+            finally
+            {
+                await stream.DisposeAsync();
+            }
+            Error = null;
+            TaskStatus = TaskStatus.RanToCompletion;
+            Finished?.Invoke(this, result);
         }
     }
 }
